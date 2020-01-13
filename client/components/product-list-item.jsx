d4c8c9d5327dae602cbc5de8007318579eb1a594
david <dholic9@github.com>
2020-01-13 23:04:55 +0000

    completed front end view products feature

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+export default class ProductListItem extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <div
+        className="card   align-items-center justify-content-center"
+        style={{
+          width: "100%",
+          height: "33rem"
+        }}>
+          <img src={this.props.image} height="250px" width="100%" className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5>{this.props.name}</h5>
+              <p className="card-text">{this.props.shortDescription}</p>
+              <p>{"$"+this.props.price}</p>
+            </div>
+      </div>
+    )
+  }
+}
