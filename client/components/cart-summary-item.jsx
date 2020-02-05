0261333257c18cb4d982d00acdcbd6d32e144b5b
david <daviddhongg@gmail.com>
2020-02-05 22:26:02 +0000

    added media queries for iphone 6/7/8 portrait/landscape

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -8,7 +8,7 @@ export default class CartSummaryItem extends React.Component {
         <div className="col-5">
           <img height="250px" width="300px" src={this.props.item.image} alt=""/>
         </div>
-        <div className="ml-4 col-6">
+        <div className="cart-item-detail col-sm-12 col-md-6">
           <h3>{this.props.item.name}</h3>
           <h5 className="text-secondary">{'$' + (this.props.item.price / 100).toFixed(2)}</h5>
           <p>{this.props.item.shortDescription}</p>
