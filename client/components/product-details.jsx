40123d48a460b4a05ab357a9b1a392a5abfe220f
david <daviddhongg@gmail.com>
2020-02-25 03:08:41 +0000

    finished add to cart confirm modal

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -4,11 +4,14 @@ export default class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: null
+      product: null,
+      showModal: false,
     };
     this.productId = this.props.params.productId;
     this.handleCatalogClick = this.handleCatalogClick.bind(this);
     this.handleAddCart = this.handleAddCart.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.goToCart = this.goToCart.bind(this)
   }
 
   componentDidMount() {
@@ -26,26 +29,36 @@ export default class ProductDetails extends React.Component {
     this.props.setView('catalog', {});
   }
 
+  closeModal(){
+    this.setState({showModal: false})
+  }
+
+  goToCart(){
+    this.props.setView('cart', {})
+  }
+
+
   handleAddCart() {
     this.props.addToCart(this.state.product);
+    this.setState({showModal: true})
+
   }
 
   render() {
     return (
       <div className="product-detail mx-4 mb-5 fadeIn">
-        <div className="row flex-row">
+        <div className="row flex-row clickable">
           <i onClick={this.handleCatalogClick} className="fas fa-angle-left d-flex text-secondary my-4 mr-2 ml-2"></i>
           <div onClick={this.handleCatalogClick} className="backButton my-3 text-secondary ">  Back to catalog</div>
         </div>
-
         <div className="row card-body ">
-          <div className="product-detail-image text-center col-sm-12 col-md-5 p-0 mb-4">
+          <div className="product-detail-image text-center col-sm-12 col-lg-7 p-0 pr-3 mb-4">
             <img className="card-detail-image" src={this.state.product
               ? this.state.product.image
               : 'loading'} alt=""
             />
           </div>
-          <div className="col-md-7 col-sm-12 card-detail">
+          <div className="col-md-12 col-lg-5 col-sm-12 mb-3 card-detail">
             <h2>{this.state.product
               ? this.state.product.name
               : 'loading'}</h2>
@@ -64,6 +77,27 @@ export default class ProductDetails extends React.Component {
               : 'loading'}</p>
           </div>
         </div>
+
+        <div className={this.state.showModal
+          ? "modal longFadeIn start-modal text-center"
+          : "modal longFadeIn hidden start-modal text-center"}>
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content add-cart-modal align-items-center text-center justify-content-center">
+              <div className="modal-body p-0  flex-column justify-content-center">
+                <h5 className='my-4 mx-5 '>Item has been added to your cart</h5>
+                <div className=" my-3 mx-2 row flex-row justify-content-around">
+                  <div className="col-6">
+                    <button className="btn btn-primary continue-button" onClick={this.closeModal} data-dismiss="modal">Continue Shopping</button>
+                  </div>
+                  <div className="col-6">
+                    <button className="btn btn-danger  go-to-cart-button"  onClick={this.goToCart} data-dismiss="modal">Go To Cart</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
